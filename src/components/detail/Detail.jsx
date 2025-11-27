2fb35930a469dfefe401c5dbcd8da6afc735714c
KusalyaDeZoysa <129369340+KusalyaDeZoysa@users.noreply.github.com>
2025-11-27 21:58:39 +0530

    contact details section

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,9 +1,13 @@
 import './detail.css'
 
 export default function Detail() {
-  return (
-    <div className="detail">
-      detail
-    </div>
-  )
+    return (
+        <div className="detail">
+            <div className='user'>
+                <img src="./avatar.png" alt="" />
+                <p>John Doe</p>
+            </div>
+            <button className='block'>Block User</button>
+        </div>
+    )
 }
